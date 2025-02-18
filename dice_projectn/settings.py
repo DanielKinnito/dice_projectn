b987394f6ce56a216298fe49b3167f33bfeb8bef
DanielKinnito <mokonnendaniel@gmail.com>
2025-02-18 05:45:14 +0300

    :bug: Add cors

--- dice_projectn/settings.py
+++ dice_projectn/settings.py
@@ -36,6 +36,7 @@ D6_API_KEY = os.getenv("D6_API_KEY") # Access the D6_API_KEY
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'dice',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Your React development server
+    "https://diceforge.vercel.com",  # Add your production domain here
+]
+
+CORS_ALLOW_CREDENTIALS = True  # If you need to allow credentials (cookies, etc.)
